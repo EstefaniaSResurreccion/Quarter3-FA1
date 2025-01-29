cfb8d943c9977047d31c1c3844cfa57826144ded
EstefaniaSResurreccion <estefania.resurreccion@smc.pshs.edu.ph>
2025-01-29 21:12:59 +0800

    Update Number 1.js

--- Number 1.js
+++ Number 1.js
@@ -5,6 +5,7 @@ var weight = 42;
 var feet = Math.floor( height / 12);
 var inches = height % 12;
 
+var wLbs = weight * 2.20462;
 
-alert("Name: "+ nickname +  "\n Height: " + feet + "\'" +  inches + "\"" + "\n Weight: " + weight + "kg" );
+alert("Name: "+ nickname +  "\n Height: " + feet + "\'" +  inches + "\"" + "\n Weight: " + wLbs + "lbs" );
 
